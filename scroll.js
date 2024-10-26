856d3233ff36f108c603c788c933fd01043b476a
Gülüzar KARAKOÇ <karakoc.guluzar99@gmail.com>
2024-10-27 00:55:29 +0300

    Feature: Static website created.

---
+++ scroll.js
@@ -0,0 +1,11 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function showTooltip() {
+    document.getElementById('tooltip').style.display = 'block';
+}
+
+function hideTooltip() {
+    document.getElementById('tooltip').style.display = 'none';
+}
